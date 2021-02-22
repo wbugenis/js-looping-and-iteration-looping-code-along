73cdc48b24eab3ba167ce8547eb5c32c8c3463ff
William Bugenis <wbugenis@gmail.com>
2021-02-21 21:05:27 -0500

    commit tree 991b9a5a1b7c809e8952b05af17a7bbf1f76423a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let arr = [];
+    for(let num = 0; num < names.length ; num++ ) {  
+        arr.push(`Thank you, ${names[num]}, for the wonderful ${event} gift!`);
+    }
+    return arr;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
